c9d0a4eb6428a6bf105720fbd2349fce70823fbf
Felipe Lobo <felipeblobo@gmail.com>
2021-01-05 16:51:12 -0300

    Montagem de todas as rotas do CRUD
    Sem validação ainda

--- db.js
+++ db.js
@@ -1,8 +1,31 @@
 const mongoClient = require('mongodb').MongoClient;
 mongoClient.connect("mongodb://localhost", { useUnifiedTopology: true}).then(conn => global.conn = conn.db("nodemongo")).catch(err => console.log(err))
 
-function findAll(callback) {
-    global.conn.collection("clientes").find({}).toArray(callback);
+var ObjectId = require('mongodb').ObjectId;
+
+// função para inserir um registro de cliente
+function insertOne(cliente, callback) {
+    global.conn.collection('clientes').insertOne(cliente, callback);
 }
 
-module.exports = {findAll}
+// função para procurar todos os clientes
+function findAll(callback) {
+    global.conn.collection('clientes').find({}).toArray(callback);
+}
+
+// função para encontrar cliente específico
+function findOne(id, callback) {
+    global.conn.collection('clientes').find(new ObjectId(id)).toArray(callback);
+}
+
+// função para editar cliente específico
+function update(id, cliente, callback) {
+    global.conn.collection('clientes').updateOne({_id: new ObjectId(id)},{$set: cliente}, callback);
+}
+
+// função para deletar cliente específico
+function deleteOne(id, cliente, callback) {
+    global.conn.collection('clientes').deleteOne({_id: new ObjectId(id)}, callback);
+}
+
+module.exports = {findAll, insertOne, findOne, update, deleteOne}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,60 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
+// CREATE clientes
+router.get("/novo", (req, res, next) => {
+  res.render("novo", { title: "Novo Cadastro de Cliente", doc: {"nome":"","idade":"","profissão":""}, action: '/novo' });
+}); 
+
+router.post("/novo", (req, res) => {
+  let nome = req.body.nome;
+  let idade = parseInt(req.body.idade);
+  let profissão = req.body.profissão;
+  global.db.insertOne({nome, idade, profissão}, (err, result) => {
+    if (err) { return console.log(err);}
+    res.redirect('/');
+  });
+});
+
+// READ ALL clientes
+router.get("/", (req, res) => {
   global.db.findAll((e, docs) => {
-    if(e) { return console.log(e);}  
-  res.render('index', { title: 'Lista de Clientes', docs: docs });
+    if (e) {
+      return console.log(e);
+    }
+    res.render("index", { title: "Lista de Clientes", docs: docs });
+  });
+});
+
+// READ UM CLIENTE
+router.get('/edit/:id', (req, res, next) => {
+  let id = req.params.id;
+  global.db.findOne(id, (e,docs) => {
+    if(e) {return console.log(e);}
+    res.render('novo', {title: 'Edição de Cliente', doc: docs[0], action:'/edit/' + docs[0]._id});
   })
 });
 
-module.exports = router;
+
+// EDITANDO UM CLIENTE
+router.post('/edit/:id', (req, res) => {
+  let id = req.params.id;
+  let nome = req.body.nome;
+  let idade = parseInt(req.body.idade);
+  let profissão = req.body.profissão;
+  global.db.update(id, {nome, idade, profissão}, (e, result) =>{
+    if(e) {return console.log(e);}
+    res.redirect('/');
+  });
+});
+
+// DELETANDO UM CLIENTE
+router.post('/delete/:id', (req,res) => {
+  let id = req.params.id;
+  global.db.deleteOne(id, (e,r) => {
+    if(e) {return console.log(e);}
+    res.redirect('/');
+  });
+});
+
+module.exports = router
